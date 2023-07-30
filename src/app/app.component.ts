e3730382258e2341268241a9c1e43796007040b3
bastienlhy <bastien.lahaye@hotmail.fr>
2023-07-30 17:33:35 +0200

    Init project structure: use of standalone components, header and content components created with sidenav module for the content.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, ContentComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'online-resume';
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [MatSidenavModule, MatListModule],
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent {
+
+} 
