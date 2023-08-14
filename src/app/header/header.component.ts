5280233ebaf89ab2109ca9fc563fb6587bcf7ff9
law-millenium <102876095+law-millenium@users.noreply.github.com>
2023-08-14 17:02:53 +0200

    chore(linter-prettier): :art: Add prettier and linter rules on files save. (#5)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,10 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
