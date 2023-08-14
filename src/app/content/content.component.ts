4a5ad5ec2a1dd58801af12b52b65ecbcc7802d27
law-millenium <102876095+law-millenium@users.noreply.github.com>
2023-08-14 17:02:53 +0200

    chore(linter-prettier): :art: Add prettier and linter rules on files save. (#5)

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -3,12 +3,10 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-content',
-  standalone: true,
-  imports: [MatSidenavModule, MatListModule],
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+    selector: 'app-content',
+    standalone: true,
+    imports: [MatSidenavModule, MatListModule],
+    templateUrl: './content.component.html',
+    styleUrls: ['./content.component.scss']
 })
-export class ContentComponent {
-
-} 
+export class ContentComponent {}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
-
 bootstrapApplication(AppComponent, {
-    providers: [
-        provideAnimations(),
-    ],
-}).catch(err => console.error(err));
+    providers: [provideAnimations()]
+}).catch((err) => console.error(err));
