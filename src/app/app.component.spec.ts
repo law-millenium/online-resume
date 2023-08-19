d5f212bbd8f8a10a6d3ba4279042f3c042d62ccf
law-millenium <102876095+law-millenium@users.noreply.github.com>
2023-08-19 14:22:21 +0200

    feat(header): :sparkles: Realize responsive and accessible header. (#4)
    Note: Stylelint added and linked to prettier in order to format SCSS files on save.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('online-resume app is running!');
+        expect(compiled.querySelector('.content span')?.textContent).toContain(
+            'online-resume app is running!'
+        );
     });
 });
